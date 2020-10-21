c5a5f705240bfcffcd88c772d5fba9495ce41ebe
Alexander Kropivnyy <kropivnyy@gmail.com>
2020-10-21 23:47:57 +0300

    added variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'modern-normalize/modern-normalize.css';
+import './variables.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
