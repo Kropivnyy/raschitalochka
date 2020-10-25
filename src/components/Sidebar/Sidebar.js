d7f8f80ab877594b846dcd7ef5d83d82273608ea
Victor Batigin <bixs85@gmail.com>
2020-10-25 15:08:57 +0200

    added content in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import Navigation from '../Navigation';
+import Currency from '../../views/Currency';
 import styles from './Sidebar.module.css';
+import { NavLink } from 'react-router-dom';
+import routes from '../../routes';
 
 export default function Sidebar() {
-  return <aside></aside>;
+  return (
+    <>
+      <div className={styles.mb}>
+        <NavLink
+          to={routes.homePageView}
+          exact
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          Home page
+        </NavLink>
+        <NavLink
+          to={routes.statisticsView}
+          exact
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          Diagram
+        </NavLink>
+      </div>
+
+      <div className={styles.mb}>Total Balance, UAH 24 000.00</div>
+
+      <Currency />
+    </>
+  );
 }
